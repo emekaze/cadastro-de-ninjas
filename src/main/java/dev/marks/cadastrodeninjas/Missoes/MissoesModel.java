5f59ef50a37ccf7ce62ec5f4424863e723cefd2c
MarksCardoso <marks.dev@hotmail.com>
2025-04-11 03:57:33 -0300

    utilização de Lombok para criação de constructors, getters e setters em NinjaModel e MissoesModel.

--- src/main/java/dev/marks/cadastrodeninjas/Missoes/MissoesModel.java
+++ src/main/java/dev/marks/cadastrodeninjas/Missoes/MissoesModel.java
@@ -3,11 +3,17 @@ package dev.marks.cadastrodeninjas.Missoes;
 
 import dev.marks.cadastrodeninjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
@@ -19,45 +25,5 @@ public class MissoesModel {
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninja;
 
-    public MissoesModel() {
-    }
 
-    public MissoesModel(Long id, String nomeMissao, String rank, List<NinjaModel> ninja) {
-        this.id = id;
-        this.nomeMissao = nomeMissao;
-        this.rank = rank;
-        this.ninja = ninja;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNomeMissao() {
-        return nomeMissao;
-    }
-
-    public void setNomeMissao(String nomeMissao) {
-        this.nomeMissao = nomeMissao;
-    }
-
-    public String getRank() {
-        return rank;
-    }
-
-    public void setRank(String rank) {
-        this.rank = rank;
-    }
-
-    public List<NinjaModel> getNinja() {
-        return ninja;
-    }
-
-    public void setNinja(List<NinjaModel> ninja) {
-        this.ninja = ninja;
-    }
 }
